8511226fdfce91e4eaaeb3f25e2d5cb039003c03
Mihai <mihaighiga14@icloud.com>
2024-06-01 16:56:31 +0100

    adding iamge and nutritional info

--- public/main.js
+++ public/main.js
@@ -1,54 +1,128 @@
-$(document).ready(function() {
-    // Arrays storing the information about the toppings
-    const fruits = [
-        { name: 'Banana', image: 'https://via.placeholder.com/100?text=Banana', calories: 0.89, fat: 0.3, protein: 1.1, sugar: 12.2 },
-        { name: 'Strawberry', image: 'https://via.placeholder.com/100?text=Strawberry', calories: 0.32, fat: 0.3, protein: 0.8, sugar: 4.9 },
-        { name: 'Blueberry', image: 'https://via.placeholder.com/100?text=Blueberry', calories: 0.57, fat: 0.3, protein: 0.7, sugar: 9.7 },
-        { name: 'Apple', image: 'https://via.placeholder.com/100?text=Apple', calories: 0.52, fat: 0.2, protein: 0.3, sugar: 10.4 },
-        { name: 'Kiwi', image: 'https://via.placeholder.com/100?text=Kiwi', calories: 0.61, fat: 0.5, protein: 1.1, sugar: 9.0 }
-    ];
+$(document).ready(function () {
+  // Arrays storing the information about the toppings
+  const fruits = [
+    {
+      name: "Banana",
+      image: "https://via.placeholder.com/100?text=Banana",
+      calories: 1,
+      fat: 0,
+      protein: 0.01,
+      sugar: 0.12,
+    },
+    {
+      name: "Strawberry",
+      image: "https://c02.purpledshub.com/uploads/sites/41/2023/09/GettyImages_154514873.jpg?w=1029&webp=1",
+      calories: 6,
+      fat: 0.05,
+      protein: 0.12,
+      sugar: 0.84,
+    },
+    {
+      name: "Blueberry",
+      image: "https://www.acquagarden.co.uk/cdn/shop/products/fruit-plants-blueberry-sunshine-blue-1-x-full-plant-in-a-2-litre-pot-975576.jpg?v=1647889418",
+      calories: 1,
+      fat: 0,
+      protein: 0.01,
+      sugar: 0.01,
+    },
+    {
+      name: "Apple",
+      image: "https://www.collinsdictionary.com/images/full/apple_158989157.jpg",
+      calories: 1,
+      fat: 0,
+      protein: 0,
+      sugar: 0.1,
+    },
+    {
+      name: "Kiwi",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwQjAfVMSv0rvTIJl97anIxiSC29qJZKjXSg&s",
+      calories: 1,
+      fat: 0.01,
+      protein: 0.01,
+      sugar: 0.09,
+    },
+  ];
 
-    const otherToppings = [
-        { name: 'Honey', image: 'https://via.placeholder.com/100?text=Honey', calories: 3.04, fat: 0, protein: 0.06, sugar: 82.4 },
-        { name: 'Dark Chocolate', image: 'https://via.placeholder.com/100?text=Dark+Chocolate', calories: 5.46, fat: 30.0, protein: 7.9, sugar: 24.0 },
-        { name: 'Protein Powder', image: 'https://via.placeholder.com/100?text=Protein+Powder', calories: 4.00, fat: 1.0, protein: 20.0, sugar: 1.0 }
-    ];
+  const otherToppings = [
+    {
+      name: "Honey",
+      image: "https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/04/honey-1296x728-header.jpg?w=1155&h=1528",
+      calories: 3,
+      fat: 0,
+      protein: 0,
+      sugar: 0.82,
+    },
+    {
+      name: "Dark Chocolate",
+      image: "https://via.placeholder.com/100?text=Dark+Chocolate",
+      calories: 5.05,
+      fat: 0.34,
+      protein: 0.04,
+      sugar: 0.51,
+    },
+    {
+      name: "Protein Powder",
+      image: "https://via.placeholder.com/100?text=Protein+Powder",
+      calories: 4.0,
+      fat: 0.09,
+      protein: 0.31,
+      sugar: 0.1,
+    },
+  ];
 
-    const bowlSizes = [
-        { size: 'Small', grams: 20 ,skyr: 200},
-        { size: 'Medium', grams: 40 , skyr: 300},
-        { size: 'Large', grams: 60 , skyr: 400},
-    ];
+  const bowlSizes = [
+    { size: "Small", grams: 20, skyr: 200 },
+    { size: "Medium", grams: 40, skyr: 300 },
+    { size: "Large", grams: 60, skyr: 400 },
+  ];
 
-    // Function to generate a random skyr bowl
-    function generateSkyrBowl() {
-        const randomFruit = fruits[Math.floor(Math.random() * fruits.length)];
-        const randomOtherTopping = otherToppings[Math.floor(Math.random() * otherToppings.length)];
-        const randomBowlSize = bowlSizes[Math.floor(Math.random() * bowlSizes.length)];
+  // Function to generate a random skyr bowl
+  function generateSkyrBowl() {
+    const randomFruit = fruits[Math.floor(Math.random() * fruits.length)];
+    const randomOtherTopping =
+      otherToppings[Math.floor(Math.random() * otherToppings.length)];
+    const randomBowlSize =
+      bowlSizes[Math.floor(Math.random() * bowlSizes.length)];
 
-        const fruitAmountInSize = randomBowlSize.grams;
-        const fruitCalories = (randomFruit.calories * fruitAmountInSize).toFixed(2);
-        const fruitFat = (randomFruit.fat * fruitAmountInSize).toFixed(2);
-        const fruitProtein = (randomFruit.protein * fruitAmountInSize).toFixed(2);
-        const fruitSugar = (randomFruit.sugar * fruitAmountInSize).toFixed(2);
+    const fruitAmountInSize = randomBowlSize.grams;
+    const fruitCalories = (randomFruit.calories * fruitAmountInSize).toFixed(2);
+    const fruitFat = (randomFruit.fat * fruitAmountInSize).toFixed(2);
+    const fruitProtein = (randomFruit.protein * fruitAmountInSize).toFixed(2);
+    const fruitSugar = (randomFruit.sugar * fruitAmountInSize).toFixed(2);
 
-        const otherToppingAmount = randomBowlSize.grams;
-        const otherToppingCalories = (randomOtherTopping.calories * otherToppingAmount).toFixed(2);
-        const otherToppingFat = (randomOtherTopping.fat * otherToppingAmount).toFixed(2);
-        const otherToppingProtein = (randomOtherTopping.protein * otherToppingAmount).toFixed(2);
-        const otherToppingSugar = (randomOtherTopping.sugar * otherToppingAmount).toFixed(2);
+    const otherToppingAmount = randomBowlSize.grams;
+    const otherToppingCalories = (
+      randomOtherTopping.calories * otherToppingAmount
+    ).toFixed(2);
+    const otherToppingFat = (
+      randomOtherTopping.fat * otherToppingAmount
+    ).toFixed(2);
+    const otherToppingProtein = (
+      randomOtherTopping.protein * otherToppingAmount
+    ).toFixed(2);
+    const otherToppingSugar = (
+      randomOtherTopping.sugar * otherToppingAmount
+    ).toFixed(2);
 
-        let result = `
-            <p>Base: Skyr ${randomBowlSize.skyr}</p>
-            <img src="${randomFruit.image}" alt="${randomFruit.name}">
-            <p>Fruit Topping: ${randomFruit.name} - ${fruitAmountInSize}g</p>
-            <img src="${randomOtherTopping.image}" alt="${randomOtherTopping.name}">
-            <p>Other Topping: ${randomOtherTopping.name} - ${otherToppingAmount}g</p>
+    let result = `
+        
+        <div class="card me-3" style="width: 18rem;">
+        <img src="${randomFruit.image}" class="card-img-top" alt="${randomFruit.name}">
+            <div class="card-body">
+            <p><b>Fruit Topping:</b> ${randomFruit.name} - ${fruitAmountInSize}g</p>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+        <img src="${randomOtherTopping.image}" class="card-img-top" alt="${randomOtherTopping.name}">
+            <div class="card-body">
+            <p><b>Other Topping:</b> ${randomOtherTopping.name} - ${otherToppingAmount}g</p>
+            </div>
+        </div>
         `;
 
-        $('#result').html(result);
-
-        let nutritionTable = `
+    $("#result").html(result);
+    $(".skyr-bowl-size").html(`<h5 class="card-title mt-2 fw-bold">${randomBowlSize.skyr}g of Skyr</h5>`);
+    let nutritionTable = `
             <tr>
                 <td>${randomFruit.name}</td>
                 <td>${fruitAmountInSize}</td>
@@ -67,11 +141,11 @@ $(document).ready(function() {
             </tr>
         `;
 
-        $('#nutritionTableBody').html(nutritionTable);
-    }
+    $("#nutritionTableBody").html(nutritionTable);
+  }
 
-    // Event listener for the generate button
-    $('#generateButton').click(function() {
-        generateSkyrBowl();
-    });
+  // Event listener for the generate button
+  $("#generateButton").click(function () {
+    generateSkyrBowl();
+  });
 });
